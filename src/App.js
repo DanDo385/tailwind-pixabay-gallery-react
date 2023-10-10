ff39e3301072663280db09faabadcb1b96bd7bef
Daniel Magro <dando385@yahoo.com>
2023-10-10 14:02:51 -0400

    fixed export default app by closing function with closing curly brace }

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard"; 
 
-
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -16,21 +15,16 @@ function App() {
     })
     .catch(err => console.log(err));
   }, []);
-
-
-
-   
-
-
+ 
   return (
     <div className = "container mx-auto">
-      <div className= "grid.grid-cols-3.gap-4"
+      <div className = "grid grid-cols-3 gap-4">
         {images.map(image => (
-          <ImageCard key = {image.id} image = {image}/>
+          <ImageCard key = {image.id} image = {image} />
         ))}
       </div>
     </div>
   );
-   
+}
 
 export default App;
