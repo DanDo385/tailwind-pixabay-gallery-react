0fa56deaafdc8f164462b66ee92e77b160d54f34
Daniel Magro <dando385@yahoo.com>
2023-10-10 15:16:44 -0400

    added imagesearch button and styled using twcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard"; 
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -18,13 +19,17 @@ function App() {
  
   return (
     <div className = "container mx-auto">
-      <div className = "grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key = {image.id} image = {image} />
-        ))}
-      </div>
+      <ImageSearch />
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
