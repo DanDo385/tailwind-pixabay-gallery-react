c3db4ae627212787c6a772a1904a3d138106310d
Daniel Magro <dando385@yahoo.com>
2023-10-10 12:10:30 -0400

    added useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json());
+   });
+
+
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src='https://source.unsplash.com/random' alt='' className='w-full' />
-      <div className='px-6 py-4'>
-        <div className='font-bold text-purple-500 text-xl mb-2'>
+    <div className = "max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by John Doe
         </div>
         <ul>
           <li>
-            <strong>Views:</strong> 4000
+            <strong>Views: </strong>
+            4000
           </li>
           <li>
-            <strong>Downloads:</strong> 300
+            <strong>Downloads: </strong>
+            300
           </li>
           <li>
-            <strong>Likes:</strong> 400
+            <strong>Likes: </strong>
+            400
           </li>
-        </ul>
+        </ul> 
+
       </div>
     </div>
   );
